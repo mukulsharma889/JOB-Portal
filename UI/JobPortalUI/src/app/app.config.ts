b7b796e4ade86df08a8d74b08c88198672fe20ab
mukulsharma889 <smukul889@gmail.com>
2026-02-08 02:10:22 +0530

    fix : UI

--- UI/JobPortalUI/src/app/app.config.ts
+++ UI/JobPortalUI/src/app/app.config.ts
@@ -1,8 +1,21 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import Nora from '@primeuix/themes/nora';
+import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Nora,
+        options: {
+          darkModeSelector: '.my-app-dark', // Optional: for dark mode toggling
+        },
+      },
+    }),
+  ],
 };
